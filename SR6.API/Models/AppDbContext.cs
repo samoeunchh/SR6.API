b08d3f9026f163e9f0078fb01749e4d192943f1e
samoeun chhay <samoeunchh@gmail.com>
2021-09-29 10:44:35 +0700

    Add project files.

---
+++ SR6.API/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SR6.API.Models
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Brand> Brand { get; set; }
+    }
+}
---
+++ SR6.API/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR6.API.Models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid BrandId { get; set; }
+        [Required(ErrorMessage ="Brand name field is required")]
+        [MaxLength(50)]
+        public string BrandName { get; set; }
+        [MaxLength(100)]
+        public string Description { get; set; }
+    }
+}
